e7bbe52c61ae0c8ee1dd67490b82ca4ac9817734
aayushks <aayushks@amazon.com>
2017-09-24 09:39:36 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 from setuptools import setup
-
+from setuptools import find_packages
 setup(
     name='pirant',
-    version='0.1',
-    scripts=['pirant']
+    version='0.1.0devl',
+    author = "aayush",
+    url="https://github.com/aayush26/pirant",
+    license='MIT',
+    description = "DevRant wrapper in Python",
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords="devrant api wrapper",
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=[
+        "colander",
+        "requests",
+    ]
 )
