2f2535353f54b5a0eda658a8ff25d78f5f2d6848
Jose Ramon Cano <joserc87@gmail.com>
2017-10-01 11:26:56 +0200

    Move content of app.py to __init__.py
    By having the main code in __init__.py we can import the library like:
    
    from pirant import DevRant
    
    instead of:
    
    from pirant.app import DevRant

---
+++ pirant/__init__.py
@@ -0,0 +1,12 @@
+from .utils.RequestHandler import RequestHandler
+from .utils.ResponseHandler import ResponseHandler
+
+class DevRant:
+
+    def __init__(self):
+        self.RequestHandler = RequestHandler()
+        self.ResponseHandler = ResponseHandler()
+
+    def getRants(self, sort, limit, skip):
+        response = self.RequestHandler.getRants(sort, limit, skip)
+        return self.ResponseHandler.getRantsBuildResponse(response)
