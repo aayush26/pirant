3b70355c175c8d0a1c63a27a3728c4dd624b8921
aayushks <aayushks@amazon.com>
2017-10-01 16:31:35 +0530

    v0.1.4dev1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 setup(
     name='pirant',
-    version='0.1.3dev1',
+    version='0.1.4dev1',
     author = "aayush",
     author_email='aayushsingh.mail@gmail.com',
     url="https://github.com/aayush26/pirant",
